675a18edb57752ad19d5712f528cc347a25ad294
maetsuyo <maenoorehatuyokatta8009@gmail.com>
2024-10-03 16:03:08 +0900

    画面遷移をhrefじゃなくてrouter.pushを使う

--- src/app/components/Login_Form.tsx
+++ src/app/components/Login_Form.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FormEvent } from "react";
 
 interface LoginFormProps {
@@ -10,31 +11,45 @@ interface LoginFormProps {
 
 const LoginForm = ({id, pass, onChangeId, onChangePass, onSubmitForm}: LoginFormProps) => {
   return (
-    <form onSubmit={onSubmitForm} className="text-center pb-10">
-      <p>
+    <form onSubmit={onSubmitForm} className="bg-white shadow-lg rounded-lg p-10 max-w-xs mx-auto mt-12 border border-gray-300">
+      <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">LOGIN</h1>
+      <div className="mb-6">
         <input
           type="text"
           name="id"
           placeholder="id"
           value={id}
           onChange={onChangeId}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg p-2.5"
+          className="bg-gray-100 border border-gray-300 text-gray-900 rounded-lg p-4 w-full transition duration-300 ease-in-out transform focus:border-blue-500 focus:ring focus:ring-blue-200 hover:shadow-md"
         />
-      </p>
-      <p>
+      </div>
+      <div className="mb-6">
         <input
           type="text"
           name="pass"
           placeholder="password"
           value={pass}
           onChange={onChangePass}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg p-2.5"
+          className="bg-gray-100 border border-gray-300 text-gray-900 rounded-lg p-4 w-full transition duration-300 ease-in-out transform focus:border-blue-500 focus:ring focus:ring-blue-200 hover:shadow-md"
         />
-      </p>
+      </div>
       <button
         type="submit"
-        className="text-white bg-blue-700 font-medium rounded-lg text-sm px-2 py-2 mt-3">LOGIN
+        className="w-full text-white bg-gradient-to-r from-blue-500 to-purple-600 font-semibold rounded-lg py-2 hover:from-blue-600 hover:to-purple-700 transition duration-300 ease-in-out text-lg"
+      >
+        LOGIN
       </button>
+      <p
+        className="text-center mt-4 text-gray-600 text-sm"
+      >
+        Don't have an account?
+        <Link
+          href="signup"
+          className="text-blue-500 hover:underline"
+        >
+          signup
+        </Link>
+      </p>
     </form>
   );
 };
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,23 @@
+export default function signup() {
+  return (
+    <>
+      <form>
+        <p>id</p>
+          <input
+            type="text"
+            name="id"
+          />
+        <p>password</p>
+          <input
+            type="text"
+            name="password"
+          />
+        <p>
+          <button type="submit">
+            SIGNUP
+          </button>
+        </p>
+      </form>
+    </>
+  );
+};
